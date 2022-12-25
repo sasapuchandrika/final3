90da2c5c16a97bea25c285fa56ae9f71f9360204
Jayasree1802 <121030796+Jayasree1802@users.noreply.github.com>
2022-12-26 00:42:45 +0530

    Update seller.c

--- seller.c
+++ seller.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include"prototype.h"
-#include"buyer_prototype.h"
+#include"header.h"
+//#include"buyer_prototype.h"
 
 void display(seller_Details *seller_root)
 {
